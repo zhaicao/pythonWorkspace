bf88464507157e9d49c45519cd50a7fedae7c1b6
zhaicao <ricky2971@hotmail.com>
2018-05-20 21:32:01 +0800

    增加pyqt5

--- Demo/exercise_1.py
+++ Demo/exercise_1.py
@@ -1,36 +1,8 @@
-#encoding=utf-8
+# -*- coding: utf-8 -*-
 
-__author__='ricky'
-
-# def trim(s):
-#     if len(s) == 0 : return s
-#     for start in range(0, len(s) - 1):
-#         if s[start] != ' ': break
-#     for end in range(len(s) - 1, 0, -1):
-#         if s[end] != ' ': break
-#     return s[start : end + 1]
+# Form implementation generated from reading ui file 'exercise_1.py'
 #
+# Created by: PyQt5 UI code generator 5.10.1
 #
-# # 测试:
-# if trim('hello  ') != 'hello':
-#     print('测试失败!')
-# elif trim('  hello') != 'hello':
-#     print('测试失败!')
-# elif trim('  hello  ') != 'hello':
-#     print('测试失败!')
-# elif trim('  hello  world  ') != 'hello  world':
-#     print('测试失败!')
-# elif trim('') != '':
-#     print('测试失败!')
-# elif trim('    ') != '':
-#     print('测试失败!')
-# else:
-#     print('测试成功!')
+# WARNING! All changes made in this file will be lost!
 
-s = {'a':'a1', 'b' : 'b1'}
-print([k + ':' + v for k,v in s.items()])
-
-L1 = ['Hello', 'World', 18, 'Apple', None]
-L2 = [x.lower() for x in L1 if isinstance(x, str)]
-
-print(L2)
