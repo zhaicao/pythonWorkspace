c0e1f677fd70b6aa534d6d910c5aa180c1414dca
zhaicao <zhaicao@zjrealtech.com>
2018-05-24 19:32:37 +0800

    增加pyqt5练习

---
+++ Pyqt5Practice/demo_7.py
@@ -0,0 +1,103 @@
+
+__author__ = 'zhaicao'
+
+import sys
+from PyQt5.QtWidgets import QWidget, QApplication, QVBoxLayout, QLCDNumber, QSlider, QPushButton, QMainWindow
+from PyQt5.QtCore import Qt, QObject, pyqtSignal
+
+
+class simple(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    #信号槽
+    def initUI(self):
+        lcd = QLCDNumber()
+        sld = QSlider(Qt.Horizontal, self)
+
+        vbox = QVBoxLayout()
+        vbox.addWidget(lcd)
+        vbox.addWidget(sld)
+
+        self.setLayout(vbox)
+
+        #QSlider的valueChanged信号，槽函数dispaly
+        sld.valueChanged.connect(lcd.display)
+
+        self.setGeometry(300, 300, 250, 150)
+        self.setWindowTitle('Signal & slot')
+        self.show()
+
+
+class simple_2(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setGeometry(300, 300, 250, 150)
+        self.setWindowTitle('Event Handler')
+        self.show()
+    #重写keyPressEvent函数
+    def keyPressEvent(self, QKeyEvent):
+        if QKeyEvent.key() == Qt.Key_Escape:
+            self.close()
+
+
+class simple_3(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        btn1 = QPushButton('Button 1', self)
+        btn1.move(30, 50)
+
+        btn2 = QPushButton('Button 2', self)
+        btn2.move(150, 50)
+
+        #绑定信号槽
+        btn1.clicked.connect(self.buttonClicked)
+        btn2.clicked.connect(self.buttonClicked)
+
+        self.statusBar()
+
+        self.setGeometry(300, 300, 300, 200)
+        self.setWindowTitle('Event Handler')
+        self.show()
+
+    def buttonClicked(self):
+        #获得信号源对象
+        sender = self.sender()
+        print(sender.width())
+        self.statusBar().showMessage(sender.text() + ' was pressed')
+
+
+class Communicate(QObject):
+    closeApp = pyqtSignal()
+
+
+class simple_4(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.c = Communicate()
+        self.c.closeApp.connect(self.close)
+
+        self.setGeometry(300, 300, 300, 200)
+        self.setWindowTitle('Event Handler')
+        self.show()
+
+    def mousePressEvent(self, event):
+        self.c.closeApp.emit()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    w = simple_4()
+    sys.exit(app.exec_())
+
+
